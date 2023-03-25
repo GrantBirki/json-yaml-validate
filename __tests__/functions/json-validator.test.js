524bf69a5267c51505f84f4009d417212faa339a
grantbirki <grantbirki@github.com>
2023-03-25 13:41:20 +0000

    add tests

--- __tests__/functions/json-validator.test.js
+++ __tests__/functions/json-validator.test.js
@@ -1,15 +1,42 @@
 import {jsonValidator} from '../../src/functions/json-validator'
+import * as core from '@actions/core'
+
+const errorMock = jest.spyOn(core, 'error').mockImplementation(() => {})
 
 beforeEach(() => {
   jest.clearAllMocks()
   process.env.INPUT_JSON_SCHEMA = './__tests__/fixtures/schemas/schema1.json'
+  process.env.INPUT_BASE_DIR = './__tests__/fixtures/json/valid'
 })
 
 test('successfully validates a json file with a schema', async () => {
-  expect(await jsonValidator()).toBe(undefined)
+  expect(await jsonValidator()).toBe(true)
 })
 
-test('successfully validates a json file with no schema', async () => {
+test('successfully validates a json file without using a schema', async () => {
   process.env.INPUT_JSON_SCHEMA = ''
-  expect(await jsonValidator()).toBe(undefined)
+  expect(await jsonValidator()).toBe(true)
+})
+
+test('fails to validate a json file without using a schema', async () => {
+  process.env.INPUT_JSON_SCHEMA = ''
+  process.env.INPUT_BASE_DIR = './__tests__/fixtures/json/invalid'
+  expect(await jsonValidator()).toBe(false)
+  expect(errorMock).toHaveBeenCalledWith(
+    'failed to parse JSON file: ./__tests__/fixtures/json/invalid/json1.json'
+  )
+})
+
+test('fails to validate a json file with an incorrect schema', async () => {
+  process.env.INPUT_JSON_SCHEMA = './__tests__/fixtures/schemas/schema2.json'
+  expect(await jsonValidator()).toBe(false)
+  expect(errorMock).toHaveBeenCalledWith([
+    {
+      instancePath: '/foo',
+      keyword: 'type',
+      message: 'must be string',
+      params: {type: 'string'},
+      schemaPath: '#/properties/foo/type'
+    }
+  ])
 })
--- src/functions/json-validator.js
+++ src/functions/json-validator.js
@@ -1,11 +1,14 @@
 import * as core from '@actions/core'
 import Ajv from 'ajv'
 import {readFileSync} from 'fs'
+import {globSync} from 'glob'
 
 const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
 
 export async function jsonValidator() {
   const jsonSchema = core.getInput('json_schema')
+  const baseDir = core.getInput('base_dir')
+  const baseDirSanitized = baseDir.replace(/\/$/, '')
 
   // if a jsonSchema is provided, validate the json against it
   var schema
@@ -19,10 +22,23 @@ export async function jsonValidator() {
 
   const validate = ajv.compile(schema)
 
-  const data = JSON.parse(
-    readFileSync('./__tests__/fixtures/json/json1.json', 'utf8')
-  )
-
-  const valid = validate(data)
-  if (!valid) console.log(validate.errors)
+  // loop through all json files in the baseDir and validate them
+  var success = true
+  const files = globSync('**/*.json', {cwd: baseDirSanitized})
+  for (const file of files) {
+    var data
+    try {
+      data = JSON.parse(readFileSync(`${baseDirSanitized}/${file}`, 'utf8'))
+    } catch {
+      core.error(`failed to parse JSON file: ${baseDirSanitized}/${file}`)
+      success = false
+      continue
+    }
+    const valid = validate(data)
+    if (!valid) {
+      core.error(validate.errors)
+      success = false
+    }
+  }
+  return success
 }
